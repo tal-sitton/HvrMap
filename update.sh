0206c352b6475ad48a53d2e0619671d3b48e9125
tal-sitton <tal.sitton04@gmail.com>
2025-11-21 17:18:07 +0200

    add vpn

--- update.sh
+++ update.sh
@@ -11,4 +11,5 @@ curl 'https://nodews.hvr.co.il/get_dataset/markers_hvr' \
   -H 'Origin: https://www.hvr.co.il' \
   -H 'Referer: https://www.hvr.co.il/site/pg/maps_hvr' \
   -H 'sec-ch-ua: "Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"' \
+  -x 127.0.0.1:8118 \
   -o ./maps_hvr_files/markers_hvr
